a5455fcaa18370ac4278392869ed41eaaa0f1970
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 00:00:36 -0300

    feat: math

---
+++ math.py
@@ -0,0 +1,12 @@
+import math
+
+pi = 3.14
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(9))
+print(max(1,2,3,4,5,23,1,3))
+print(min(1,2,3,4,5,23,1,3))
