9ffd52d569c45c76d1bce2cdcecf31e0a4a339de
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-06 23:43:48 -0300

    feat: multiple-assignments

---
+++ multiple-assignments.py
@@ -0,0 +1,13 @@
+# name = "Bro"
+# age = 21
+# attractive = True
+name, age, attractive = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+one, two, three = 1, 2, 3
+print(one)
+print(two)
+print(three)
