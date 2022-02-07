5d224e95273538900dde3277657dcfc0c1dfb041
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-06 23:50:01 -0300

    feat: string methods

---
+++ string-methods.py
@@ -0,0 +1,13 @@
+name = "bro Brawso"
+
+print(len(name))
+print(name.find("w"))
+print(name.find("k"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count('o'))
+print(name.replace('o', 'a'))
+print(name*3)
