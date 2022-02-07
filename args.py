7a00f23d37ea99008ee5db8c2e797522ad93658b
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 07:41:43 -0300

    feat: passing dynamic args to function

--- args.py
+++ args.py
@@ -6,3 +6,11 @@ def add(*stuff):
     return  sum
 
 print(add(1,1,5))
+
+def join_terms(*terms):
+    s = " "
+    for i in terms:
+        s += ' {}'.format(i)
+    return s
+
+print(join_terms("hello", "world,", "how", "are", "you"))
