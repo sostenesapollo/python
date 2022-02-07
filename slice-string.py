52a37a08abb5b47e85987160f7dc8f356d51efa8
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 00:08:23 -0300

    feat: strings

---
+++ slice-string.py
@@ -0,0 +1,12 @@
+name = "Bro Code"
+
+fisrt_name = name[:3]
+last_name = name[4:]
+
+print(fisrt_name)
+print(last_name)
+
+parts = name.split(' ')
+print(parts[0]+'.'+parts[1])
+
+print(name[::-1])
