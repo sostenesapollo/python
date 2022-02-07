90f558bffdfacb873ade66201e55cc68f2b20805
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 06:46:47 -0300

    feat: for loop with time

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+import time
+
+for i in range(0,10+1):
+    print(i)
+
+for i in range(0,10+1, 2):
+    print(i)
+
+for i in "Bro Code":
+    print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(seconds)
+print("Boom")
