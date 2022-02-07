d261bf7191711cc1a320da4fd59a3df82f02f792
Sostenes Apollo <sostenesapollo25@gmail.com>
2022-02-07 06:58:27 -0300

    feat: 2d list

---
+++ 2d-list.py
@@ -0,0 +1,9 @@
+drinks = ["coffee", "tea", "water", "juice"]
+dinner = ["pizza", "hamburguer", "hotdog"]
+dessert = ["cake", "ice cream", "pie"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
+print(food[0][1])
+print(food[2][2])
